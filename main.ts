1f0b56ab9bbb189fb97aacc09ac1178c71e29fdc
Mehak-Akram <akramabbasi019@gmail.com>
2024-04-02 20:43:50 +0500

    push my todo-list to github

---
+++ main.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let condition = true;
+
+while(condition)
+{
+let addTask = await inquirer.prompt([{ name : "todo", type : "input", message : "what you want to add in your todos?"},
+        { name : "addMore", type : "confirm", message : "Do you want to add more Todo?", default: "false" }, ]);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log("your updated todo-list: ",todos);
+}
