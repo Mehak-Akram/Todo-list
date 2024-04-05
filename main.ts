1082e205bc0b3be7222ce07395f040f5f54ab1f4
Mehak-Akram <akramabbasi019@gmail.com>
2024-04-06 00:47:19 +0500

    update todo-list

--- main.ts
+++ main.ts
@@ -7,9 +7,38 @@ let condition = true;
 
 while(condition)
 {
-let addTask = await inquirer.prompt([{ name : "todo", type : "input", message : "what you want to add in your todos?"},
-        { name : "addMore", type : "confirm", message : "Do you want to add more Todo?", default: "false" }, ]);
-todos.push(addTask.todo);
-condition = addTask.addMore
-console.log("your updated todo-list: ",todos);
+let ans = await inquirer.prompt([{ name : "select", type : "list", message : "select an opeartion", choices : ["Add", "update", "view", "delete", "exit",],}]);
+if(ans.select === "Add"){
+let addTodo = await inquirer.prompt({name : "todo", type : "input", message : "Add items in the list", validate : function(input){
+    if(input.trim() == ""){
+        return "please enter a non-empty item."
+    }
+    return true;
+}})
+if(addTodo.todo.trim() !== ""){
+todos.push(addTodo.todo);
+todos.forEach(todo => console.log(todo))
+}       
+}
+if(ans.select === "update"){
+let updateTodo = await inquirer.prompt({name : "todo", type : "list", message : "Update items in the list", choices : todos.map(item => item)})
+let addTodo = await inquirer.prompt({name : "todo", type : "input", message : "Add items in the list",})
+let newTodo = todos.filter(val => val !== updateTodo.todo);
+todos = [...newTodo,addTodo.todo];
+todos.forEach(todo => console.log(todo))     
+}
+if(ans.select === "view"){
+todos.forEach(todo => console.log(todo))
+} 
+if(ans.select === "delete"){
+let deleteTodo = await inquirer.prompt({name : "todo", type : "list", message : "select item to delet", choices : todos.map(item => item)})
+let newTodo = todos.filter(val => val !== deleteTodo.todo);
+todos = [...newTodo];
+todos.forEach(todo => console.log(todo))
+
+}
+if(ans.select === "exit"){
+console.log("Exiting program");
+condition = false;
+}
 }
